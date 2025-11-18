0ee5b89d6f3413b879df9f38676d3f1f78b2a5ab
Frank Bruzzese <bruzzese.frank@gmail.com>
2025-11-17 22:20:35 -0800

    Section 9 - Lesson 87

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,16 @@
+﻿// 
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::Instance;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager& Manager = UGameplayTagsManager::Get();
+	
+	Instance.Attributes_Secondary_Armor = Manager.AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduces damage taken, improves Block Chance"));
+
+	
+}
---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,32 @@
+﻿// 
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * AuraGameplayTags
+ *
+ * Singleton containing native Gameplay Tags
+ */
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return Instance; }
+	static void InitializeNativeGameplayTags();
+
+	FGameplayTag Attributes_Secondary_Armor;
+
+protected:
+
+private:
+	FAuraGameplayTags() = default;
+	FAuraGameplayTags(const FAuraGameplayTags&) = delete;
+	FAuraGameplayTags(FAuraGameplayTags&&) = delete;
+	FAuraGameplayTags& operator=(const FAuraGameplayTags&) = delete;
+	FAuraGameplayTags& operator=(FAuraGameplayTags&&) = delete;
+	
+	static FAuraGameplayTags Instance;
+};
