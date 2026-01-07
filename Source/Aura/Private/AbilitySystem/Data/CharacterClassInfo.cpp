95817aa61bd345e15215ebc260b1d8e52563cacd
fbruzzese <fbruzzese@riotgames.com>
2026-01-06 17:42:35 -0800

    Section 12 - Lesson 130

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+﻿// 
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass) const
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
